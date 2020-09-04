3a8e53d34f50a25be3e792b75cfcc2d876c7996a
jeremy-setiawan <39356867+jeremy-setiawan@users.noreply.github.com>
2020-09-04 13:57:52 +0700

    Create MainWindow.js

---
+++ src/MainWindow.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import Selector from './components/Selector'
+import WebSocketFetcher from './components/WebSocketFetcher'
+/*
+setInterval(function(){
+    //RandomGen(Buff_Unit_A1,timestampBuff_Unit_A1);
+    //console.log(Buff_Unit_A1[0].length);
+    //console.log('Chart Updated!');
+},1000);
+*/
+  
+class MainWindow extends Component {   
+    constructor(){
+        super();
+        this._WebSocketFetcherRef = React.createRef();
+    } 
+    render(){
+        return(
+            <div>
+            <h1>Monitoring Baterai Lab Indi</h1>
+            
+            <Selector />    
+            <WebSocketFetcher />
+            
+            </div>
+        );
+    }
+}
+
+export default MainWindow;
